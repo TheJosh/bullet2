1898d77eb88b1608acdb133a6da390986f197e1f
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2009-09-14 22:44:31 +0000

    don't use bloated Extras folder for OpenCL branch

--- Extras/MayaPlugin/install.bat
+++
@@ -1,3 +0,0 @@
-xcopy /S /Y /C /I  /h icons\*.xpm "C:\Program Files\Autodesk\Maya2008\icons"
-xcopy /S /Y /C /I  /h scripts\*.mel "C:\Program Files\Autodesk\Maya2008\scripts"
-xcopy /S /Y /C /I  /h BulletMayaPluginDebug.mll "C:\Program Files\Autodesk\Maya2008\bin\plug-ins"
