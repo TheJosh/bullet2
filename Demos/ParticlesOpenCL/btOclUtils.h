04b9bbceae65e341ec1935515b322b39c3c52797
rponom <rponom@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2009-10-24 02:33:20 +0000

    Particle demo added (NVidia OpenCL only, parameters are not tuned, work-in-progress)

---
+++ Demos/ParticlesOpenCL/btOclUtils.h
@@ -0,0 +1,21 @@
+
+#ifndef BT_OCL_UTILS_H
+#define BT_OCL_UTILS_H
+
+#ifdef __APPLE__
+#include <OpenCL/cl.h>
+
+#else
+#include <CL/cl.h>
+#endif
+
+//#define oclCHECKERROR(a, b) btAssert((a) == (b))
+#define oclCHECKERROR(a, b) if((a)!=(b)) { printf("OCL Error : %d\n", (a)); btAssert((a) == (b)); }
+
+
+void btOclPrintDevInfo(cl_device_id device);
+cl_device_id btOclGetDev(cl_context cxMainContext, unsigned int nr);
+cl_device_id btOclGetMaxFlopsDev(cl_context cxMainContext);
+char* btOclLoadProgSource(const char* cFilename, const char* cPreamble, size_t* szFinalLength);
+cl_device_id btOclGetFirstDev(cl_context cxMainContext);
+#endif //BT_OCL_UTILS_H
